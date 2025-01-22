4fa0930a60093f7994bae588ecbe6168921791e2
Gulfairus <gulfairus.aliyar@gmail.com>
2025-01-22 22:34:10 +0500

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Gulfairus Aliyar on 22.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
